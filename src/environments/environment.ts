03481980baef8a0af07d9ab4176b535c8a275a3d
Steven <st2ven.cabarcas@gmail.com>
2024-09-01 23:05:52 -0500

    fix: environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,8 +14,7 @@ export const environment = {
   //SERVICES PLANEACIÓN
   // PLANES_CRUD: 'http://localhost:8080/',
   //PLANES_MID: 'http://localhost:8080/v1/',
-  PLANEACION_EVALUACION_MID: 'http://localhost:8082/v1/',
-  // PLANEACION_EVALUACION_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/planeacion_evaluacion_mid/v1/',
+  PLANEACION_EVALUACION_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/planeacion_evaluacion_mid/v1/',
   PLANEACION_FORMULACION_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/planeacion_formulacion_mid/v1/',
   // PLANEACION_EVALUACION_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/evaluacion_mid/v1/',
   PLANES_MID_AUTH: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/planeacion_mid/v1/',
